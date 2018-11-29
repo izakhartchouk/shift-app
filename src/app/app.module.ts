8b7a5f870b5bd7e662f93e028318a8b4eebf00ca
Ivan Zakhartchouk <ivan_zakhartchouk@epam.com>
2018-11-29 11:21:24 +0300

    SA-0000: imported material animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCheckboxModule, MatButtonModule } from "@angular/material";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSidenavModule } from "@angular/material/sidenav";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './layout/app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,14 @@ import { AppComponent } from './layout/app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
